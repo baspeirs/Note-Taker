394530a7c3be6a28c6703aa5e756a32122c31551
Bryce Speirs <baspeirs@gmail.com>
2020-06-13 14:02:40 -0400

    server.js untracked, commiting now

---
+++ server.js
@@ -0,0 +1,24 @@
+// requre dependencies and files needed to operate
+const express = require("express");
+const path = require("path");
+
+// set variable for app and PORT
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// set up data parsing using app.use and convert to json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require the files for routing our pages
+require("./Develop/routing/route-html")(app);
+require("./Develop/routing/route-api")(app);
+
+// use this to make the assets "static"
+app.use("/public", express.static('./Develop/public'));
+
+// now make your server listen
+app.listen(PORT, () => {
+    console.log("App listening on PORT: " + PORT)
+});
